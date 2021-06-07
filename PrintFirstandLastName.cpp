9901342587745054ee6da42aaaf2052e1631b024
Qasim Attaullah <68436666+friday786@users.noreply.github.com>
2021-06-07 20:43:20 +0500

    initial commit

---
+++ PrintFirstandLastName.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string first_name, last_name;
+    cout << "Enter your First Name :";
+    cin >> first_name;
+    cout << "Enter your Last Name :";
+    cin >> last_name;
+
+    cout << "Your Full Name is :" << first_name << " " << last_name;
+    return 0;
+}
---
+++ PrintMaxMin.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter a Number :";
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	int maxNo=INT_MIN;
+	int minNo=INT_MAX;
+	for(int i=0;i<n;i++)
+	{
+		maxNo=max(maxNo,arr[i]);
+	   minNo=min(minNo,arr[i]);
+	}
+	cout<<"Max No is :"<< maxNo<<" And "<<" "<<" Min No is :"<< minNo<<endl;
+	return 0;
+}
